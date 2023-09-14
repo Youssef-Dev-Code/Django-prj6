e28c9c703a0e371e535e7216c7561546245b4fb2
Youssef-Dev-Code <youssefazaiez21@gmail.com>
2023-09-14 20:14:27 +0100

    c

--- Website/views.py
+++ Website/views.py
@@ -4,12 +4,28 @@ from .models import *
 from django.contrib import messages
 import seaborn as sns
 import matplotlib.pyplot as plt
-import datetime
+import datetime, os, csv
 
 Search_Input = ""
-  
-def Index_VIEW(request):
+path_Data = os.path.join(os.getcwd(), "Data")#data directory
+path_y_f = os.path.join(path_Data, "year")
+with open(file = path_y_f, mode = "r") as f: # importing year val in year file
+    Year = int(f.readline())
+    f.close()
+path_Year = os.path.join(path_Data, "{0}".format(Year))#year directory
+Months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"]
 
+def Index_VIEW(request):
+    if datetime.datetime.today().year == Year and os.path.isdir(path_Year) == False: #condition to create dirs only one time and change year val in year file
+        with open(file = path_y_f, mode = "w") as f: # writing in year file new year val
+            f.write("{0}".format(Year+1))
+            f.close()
+        os.system("mkdir {0}".format(path_Year))
+        for month in Months:
+            path_Month = os.path.join(path_Year, month)#month directory
+            os.system("mkdir {0}".format(path_Month))
+            os.system("touch {0}".format(os.path.join(path_Month, "Absences.csv")))
+            os.system("touch {0}".format(os.path.join(path_Month, "Salaires.csv")))
     emp_list = Employé.objects.all()
     global Search_Input
     Search_Input = ""
@@ -19,22 +35,18 @@ def Affectations_VIEW(request):
     emp_list0 = Employé.objects.all()
     emp_list = list()
     global Search_Input
-    i = 0;
     for emp in emp_list0:
         if (Search_Input in emp.Prénom or Search_Input in emp.Nom or Search_Input == str(emp.id) or Search_Input == str(emp.Matricule)):
             emp_list.append(emp)
-        i += 1
     return render(request=request, template_name="Affectations.html", context={"emp_list": emp_list})
 
 def Banque_VIEW(request):
     emp_list0 = Employé.objects.all()
     emp_list = list()
     global Search_Input
-    i = 0;
     for emp in emp_list0:
         if (Search_Input in emp.Prénom or Search_Input in emp.Nom or Search_Input == str(emp.id) or Search_Input == str(emp.Matricule)):
             emp_list.append(emp)
-        i += 1
     return render(request=request, template_name="Banques.html", context={"emp_list": emp_list})
 
 def CNSS_VIEW(request):
@@ -87,13 +99,39 @@ def Absences_VIEW(request):
 
 def Absence_VIEW(request):
     emp_list = Employé.objects.all()
+    con = dict()
+    con["Year"] = Year-1#adding year to use in template
     if request.method == "POST":
-        Id = request.POST.get('Absence_Individuelle')
+        Id = request.POST.get('Absence_Individuelle')#getting id from template of employee to get absence details
         for emp in emp_list:
             if str(emp.id) == Id:
-                return render(request=request, template_name="Absence.html", context={"emp": emp, "jour_travail": 26-emp.Id_Congés.Absence})
-            else:
-                print("Error")
-                return HttpResponseRedirect(redirect_to="/Absences")
+                con["emp"] = emp #adding name to use in template
+        #check in witch month we are
+        int_month_Now = datetime.datetime.today().month #int month
+        i = 0
+        for month in Months:
+            if i == int_month_Now - 1:
+                str_month_Now = Months[i] #month in letters
+                break
+            i += 1
+        path_year_now = os.path.join(path_Data, "{0}".format(datetime.datetime.today().year)) #path of this year
+        i = 0
+        for month in Months:
+            path_Month = os.path.join(path_year_now, "{0}".format(month))#Month directory
+            path_Absences = os.path.join(path_Month, "{0}".format("Absences.csv"))#Absence directory
+            with open(file = path_Absences, mode = "r") as f: # lecture des absences de chaque emp dans le fichier
+                csvreader = csv.reader(f)
+                for row in csvreader:#HANDLE ERRORS IN THIS SECTION
+                    if row[0] == Id:
+                        x = 1
+                        z = i
+                        while x < 5:
+                            con["item"+str(z+1)] = row[x]
+                            print("ok")
+                            x += 1
+                            z += 1
+                i += 4
+                f.close()
+        return render(request=request, template_name="Absence.html", context=con)
     else:
         return HttpResponseRedirect(redirect_to="/Absences")
